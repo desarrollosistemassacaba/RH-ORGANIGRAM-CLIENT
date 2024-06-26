e674be0dcf210ba195fb89a46f5cc49f844627df
Pablo Villazon <p.villazon@gmail.com>
2024-06-26 09:00:41 -0400

    agregando archivos faltantes

---
+++ build-version.js
@@ -0,0 +1,27 @@
+const { writeFileSync } = require('fs');
+const { join } = require('path');
+
+const BUILD_VERSION_PATH = join(__dirname, 'src/build-version.json');
+
+function getTimestampBasedVersion() {
+  return parseInt(new Date().getTime() / 1000);
+}
+
+let currentIteration = getTimestampBasedVersion();
+
+try {
+  currentIteration =
+    require('./build-version.json')?.currentIteration ||
+    getTimestampBasedVersion();
+} catch (e) {
+  currentIteration = getTimestampBasedVersion();
+}
+
+const buildVersion = {
+  currentIteration: currentIteration + 1,
+  buildVersion: `${
+    require('./package.json')?.version
+  }-build.${currentIteration}`,
+};
+
+writeFileSync(BUILD_VERSION_PATH, JSON.stringify(buildVersion, null, 2));
